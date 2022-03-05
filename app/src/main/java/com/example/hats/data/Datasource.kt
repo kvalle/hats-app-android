f2da88f1938bc1e814300761e12f3b250fe2bd66
Kjetil Valle <kjetil.valle@gmail.com>
2022-03-06 00:38:47 +0100

    add dummy datasource

---
+++ app/src/main/java/com/example/hats/data/Datasource.kt
@@ -0,0 +1,26 @@
+package com.example.hats.data
+
+import com.example.hats.model.HatType
+
+class Datasource {
+
+    fun loadHats(): List<HatType> {
+        return listOf(
+            HatType("Porkpie", "Felt hat with low flat crown and narrow brim."),
+            HatType(
+                "Bowler",
+                "A hard felt hat with a rounded crown created in 1850 by Lock's of St James's, the hatters to Thomas Coke, 2nd Earl of Leicester, for his servants. More commonly known as a Derby in the United States. "
+            ),
+            HatType("Bucket hat", "A soft cotton hat with a wide, downwards-sloping brim."),
+            HatType(
+                "Top hat",
+                "Also known as a beaver hat, a magician's hat, or, in the case of the tallest examples, a stovepipe (or pipestove) hat. A tall, flat-crowned, cylindrical hat worn by men in the 19th and early 20th centuries, now worn only with morning dress or evening dress. Cartoon characters Uncle Sam and Mr. Monopoly are often depicted wearing such hats. Once made from felted beaver fur. "
+            ),
+            HatType(
+                "Fez",
+                "Red felt hat in the shape of a truncated cone, common to Arab-speaking countries."
+            )
+        )
+    }
+
+}
