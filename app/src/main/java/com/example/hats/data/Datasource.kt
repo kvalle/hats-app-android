c2e4f420e9a72e8cc9a59c740f547585a92735b7
Kjetil Valle <kjetil.valle@gmail.com>
2022-03-07 18:33:51 +0100

    add better images

--- app/src/main/java/com/example/hats/data/Datasource.kt
+++ app/src/main/java/com/example/hats/data/Datasource.kt
@@ -7,12 +7,48 @@ class Datasource {
 
     fun loadHats(): List<HatType> {
         return listOf(
-            HatType(R.string.fedora, R.string.fedora_description, R.string.fedora_image_attribution, R.drawable.fedora),
-            HatType(R.string.porkpie, R.string.porkpie_description, R.string.porkpie_image_attribution, R.drawable.porkpie),
-            HatType(R.string.bowler, R.string.bowler_description, R.string.bowler_image_attribution, R.drawable.bowler),
-            HatType(R.string.bucket_hat, R.string.bucket_hat_description, R.string.bucket_hat_image_attribution, R.drawable.bucket_hat),
-            HatType(R.string.top_hat, R.string.top_hat_description, R.string.top_hat_image_attribution, R.drawable.tophat),
-            HatType(R.string.fez, R.string.fez_description, R.string.fez_image_attribution, R.drawable.fez)
+            HatType(
+                R.string.fedora,
+                R.string.fedora_description,
+                R.string.fedora_image_attribution,
+                R.drawable.fedora_thumb
+            ),
+            HatType(
+                R.string.bowler,
+                R.string.bowler_description,
+                R.string.bowler_image_attribution,
+                R.drawable.bowler_thumb
+            ),
+            HatType(
+                R.string.bucket_hat,
+                R.string.bucket_hat_description,
+                R.string.bucket_hat_image_attribution,
+                R.drawable.bucket_hat_thumb
+            ),
+            HatType(
+                R.string.top_hat,
+                R.string.top_hat_description,
+                R.string.top_hat_image_attribution,
+                R.drawable.top_hat_thumb
+            ),
+            HatType(
+                R.string.fez,
+                R.string.fez_description,
+                R.string.fez_image_attribution,
+                R.drawable.fez_thumb
+            ),
+            HatType(
+                R.string.taqiyah,
+                R.string.taqiyah_description,
+                R.string.taqiyah_image_attribution,
+                R.drawable.taqiyah_thumb
+            ),
+            HatType(
+                R.string.toque,
+                R.string.toque_description,
+                R.string.toque_image_attribution,
+                R.drawable.toque_thumb
+            )
         )
     }
 
